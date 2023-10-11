bcba7f4054e9050f6460c609cbd5fdc4a9008116
RochusLechler <rochus.lechler@gmail.com>
2023-10-11 12:35:07 +0200

    Code in samplerep and testing works

---
+++ samplerep/utils.py
@@ -0,0 +1,15 @@
+#simple function to be tested
+''' General utilities
+'''
+
+from typing import Union
+
+import numpy as np
+
+
+def square(input_value: Union[int, float, np.ndarray]) -> Union[float, np.ndarray]:
+    '''This function takes as input an int or a float and gives as output
+     its square as a float. Alternatively, it takes as input a np.ndarray and
+     gives as output a np.ndarray with the elements squared.'''
+
+    return input_value**2
---
+++ tests/test_utils.py
@@ -0,0 +1,39 @@
+#just copied the whole content from Mr Baldini
+
+
+# Copyright (C) 2023 luca.baldini@pi.infn.it
+#
+# For the license terms see the file LICENSE, distributed along with this
+# software.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+""" Unit test for the samplerep.utils function 'square'
+"""
+
+import numpy as np
+
+from samplerep.utils import square
+
+def test_square():
+    """Unit test for the square() function.
+    """
+    assert square(2.) == 4.
+    assert square(-2.) == 4.
+    assert square(0.) == 0.
+    assert square(2) == 4
+    assert square(-2) == 4
+    assert square(0) == 0
+    assert np.allclose(square(np.ones(100)), np.ones(100))
